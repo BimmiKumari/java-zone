510cd399412f1432385b50167ed814d92238adb2
Bimmi kumari <115577045+BimmiKumari@users.noreply.github.com>
2022-11-19 20:29:24 +0530

    polygon class

---
+++ polygon.java
@@ -0,0 +1,13 @@
+package Inheritance_$_abstraction;
+
+public class Polygon {
+
+	double base;
+	double hiegth;
+	public void polygon(double base,double hiegth)
+	{
+		this.base=base;
+		this.hiegth=hiegth;
+	}
+}
+	
