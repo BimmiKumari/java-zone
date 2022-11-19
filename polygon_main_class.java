1f4e37f35c04846521b955e812731efabc58a563
Bimmi kumari <115577045+BimmiKumari@users.noreply.github.com>
2022-11-19 20:34:31 +0530

    main class of inherated subclasses triangle and rectangle

---
+++ polygon_main_class.java
@@ -0,0 +1,16 @@
+package Inheritance_$_abstraction;
+
+public class Poly_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Triangle t1=new Triangle();
+     t1.polygon(10.0,22.5);
+     t1.triangle();
+     Rectangle r1= new Rectangle();
+     r1.polygon(22.2,11.3);
+     r1.rect();
+	}
+
+}
+
