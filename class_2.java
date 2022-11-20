96059c37ca216979be56e0875d259cd116aeda9f
Bimmi kumari <115577045+BimmiKumari@users.noreply.github.com>
2022-11-20 21:25:46 +0530

    class extends from class 2

---
+++ class_2.java
@@ -0,0 +1,17 @@
+package inter_face;
+
+public class Class_2 implements Interface_2 ,Interface_1{
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("display the content");
+	}
+
+	@Override
+	public void doThat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
