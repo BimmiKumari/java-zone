91bf98a0539f7214e495448441885ed661d15e2a
Bimmi kumari <115577045+BimmiKumari@users.noreply.github.com>
2022-11-22 06:26:11 +0530

    class without mainmethod

---
+++ java_without_main.java
@@ -0,0 +1,14 @@
+package secondstep;
+
+public class Employe {
+
+	    String empname="abc";
+		int empid=100;
+		int empsalary = 5000;
+		double bonus=(empsalary*0.20); 
+	public void displayBonus() { 
+			System.out.println("employee "+empname +" gets salry of "+ empsalary+" and bonus of "+bonus);
+		}
+
+}
+
