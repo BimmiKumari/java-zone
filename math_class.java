70c5f3c9e1a369b86add7e053b0ad0432e6197ce
Bimmi kumari <115577045+BimmiKumari@users.noreply.github.com>
2022-11-23 06:07:02 +0530

    math class features

---
+++ math_class.java
@@ -0,0 +1,27 @@
+package Math_class;
+
+public class Mathclass {
+	public static void main(String args[]) {
+		int num1=4;
+		int num2=5;
+		int res=Math.min(num1, num2);
+		System.out.println(res);
+		res=Math.max(num1, num2);
+		System.out.println(res);
+		System.out.println(Math.PI);
+		System.out.println(Math.E);
+		System.out.println(Math.pow(num2, 4));
+		System.out.println(Math.sqrt(res));
+		System.out.println(Math.cbrt(res));
+		System.out.println("_________________________________");
+		
+		double x=12.98;
+		double y=10.24;
+		System.out.println(Math.ceil(y)); //increases by 1
+		System.out.println(Math.floor(y));  //decreses by 1
+		System.out.println(Math.round(y));   //perform round off
+		
+		System.out.println(Math.random());
+		
+		
+		
