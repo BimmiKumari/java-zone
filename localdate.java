1524304e0d59baab8c4156115c98290926583511
Bimmi kumari <115577045+BimmiKumari@users.noreply.github.com>
2022-11-27 01:02:11 +0530

    local date and time

---
+++ localdate.java
@@ -0,0 +1,14 @@
+package date_time;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+public class Local_date_and_time {
+
+	public static void main(String[] args) {
+	System.out.println(LocalDate.now());	
+	System.out.println(LocalTime.now());
+	System.out.println(LocalDateTime.now());
+	}
+
+}
+
